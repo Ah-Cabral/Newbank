96e5f2726f764c9abd9f174660e222171ead7e7a
AhCabral <arthurcabral.dev@gmail.com>
2021-03-03 22:46:57 -0300

    Configurando Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
-
+import NubankLogo from '../images/nubank-purple-logo.svg'
 const Header = () => {
     return (
-        <div>
+        <header className='Main-Header'>
+            <div className="Left-Side">
+                <img src={NubankLogo} alt="Logo Nubank" />
+                <button>Seu Nubank</button>
+                <button>Conta Digital</button>
+                <button>Para Você</button>
+            </div>
 
-        </div>
+            <div className="Right-Side">
+                <button>Login</button>
+                <button>Quero Ser Nubank</button>
+            </div>
+        </header>
     )
 }
 
