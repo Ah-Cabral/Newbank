2b975f16ab0ac8a88bf4f1152ff3b0fbfcc8a0c2
AhCabral <arthurcabral.dev@gmail.com>
2021-03-04 22:58:11 -0300

    Criando Cards e Exportando

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,24 +1,31 @@
 import React from 'react'
 
+import MastercardIcon from '../images/mastercard-icon.svg'
+import ChipCard from '../images/chip-card.png'
+import Signal from '../images/signal.svg'
+import NubankLogoWhite from '../images/nubank-white-logo.svg'
+
+import '../styles/components/cards.css'
+
 //Componente referente ao card normal
-const PurpleCard = () => {
+const PurpleCard = ({ name }) => {
     return (
-        <div className="simple-purple-card">
-            <div></div>
-            <div></div>
-            <div></div>
+        <div className="simple-purple-card card">
+            <div className="mastercard-div"><img src={MastercardIcon} alt="Logo-Mastercard" /></div>
+            <div className="chipcard-div"><img src={ChipCard} alt="" className="chip" /> <img src={Signal} alt="" /></div>
+            <div className="logoname-div"><img src={NubankLogoWhite} alt="" /><h3>{name}</h3></div>
         </div>
 
     )
 }
 
 //Componente referente ao black card
-const BlackCard = () => {
+const BlackCard = ({ name }) => {
     return (
-        <div className="black-purple-card">
-            <div></div>
-            <div></div>
-            <div></div>
+        <div className="black-purple-card card">
+            <div className="mastercard-div"><img src={MastercardIcon} alt="Logo-Mastercard" /></div>
+            <div className="chipcard-div"><img src={ChipCard} alt="" /> <img src={Signal} alt="" /></div>
+            <div className="logoname-div"><img src={NubankLogoWhite} alt="" /><h3>{name}</h3></div>
         </div>
 
     )
