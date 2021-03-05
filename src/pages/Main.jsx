ba49e0cded54b4fc2585fc05f6c87d95828a61a1
AhCabral <arthurcabral.dev@gmail.com>
2021-03-04 21:54:57 -0300

    Adicionando Balls

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 //Importando Ccomponente header
 import Header from '../components/Header'
+//Importando CSS
+import '../styles/css/main.css'
 
 const Main = () => {
     return (
         <>
             <Header />
+            <main className="main-container">
+                <div className="balls">
+                    <div className="gray-ball ball"></div>
+                    <div className="purple-ball ball"></div>
+                </div>
+            </main>
         </>
     )
 }
