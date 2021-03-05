afcd2354c699d1fb3054ee21abd4d69665284c0a
AhCabral <arthurcabral.dev@gmail.com>
2021-03-04 22:58:37 -0300

    Colocando cards na main

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { BlackCard, PurpleCard } from '../components/Cards'
 //Importando Ccomponente header
 import Header from '../components/Header'
 //Importando CSS
@@ -9,6 +10,8 @@ const Main = () => {
         <>
             <Header />
             <main className="main-container">
+                <BlackCard name='Arthur Cabral' />
+                <PurpleCard name='Arthur Cabral' />
                 <div className="balls">
                     <div className="gray-ball ball"></div>
                     <div className="purple-ball ball"></div>
