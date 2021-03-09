78b81c2a641ca2f6ca5ee3cf6275008499d9e638
AhCabral <arthurcabral.dev@gmail.com>
2021-03-08 23:07:47 -0300

    Inderindo toda a div para dentro da section home

--- src/pages/Main/Components/HomeSection.jsx
+++ src/pages/Main/Components/HomeSection.jsx
@@ -7,19 +7,22 @@ function homeSection() {
     return (
         <section>
             <div className="balls">
+
                 <div className="gray-ball ball"></div>
                 <div className="purple-ball ball"></div>
+
+                <section className="welcome-section">
+                    <h1>Você no controle do seu dinheiro</h1>
+                    <h3>Controle total do cartão de crédito e da conta 100% digital</h3>
+                    <button>Saiba Mais</button>
+                </section>
+
+                <div className="div-card">
+                    <PurpleCard name='Arthur Cabral' />
+                </div>
+                {/*<BlackCard name='Bem vindo ;)' />*/}
+
             </div>
-
-            {/*<BlackCard name='Bem vindo ;)' />*/}
-
-            <PurpleCard name='Arthur Cabral' />
-
-            <section className="welcome-section">
-                <h1>Você no controle do seu dinheiro</h1>
-                <h3>Controle total do cartão de crédito e da conta 100% digital</h3>
-                <button>Saiba Mais</button>
-            </section>
 
         </section>
     )
