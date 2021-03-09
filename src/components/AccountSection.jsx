c86f90b9e6ff426ff1cd6c6c480ce2d46e0d1070
AhCabral <arthurcabral.dev@gmail.com>
2021-03-09 10:58:47 -0300

    Colocando na pasta correta

---
+++ src/components/AccountSection.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+function AccountSection() {
+    return (
+        <div >
+            <div>
+                <h2>Lorem ipsum dolor sit amet</h2>
+                <h4>lorem consectueur dolor sit</h4>
+                <button>Lorem Ipsum</button>
+            </div>
+        </div>
+    )
+}
+
+export default AccountSection
