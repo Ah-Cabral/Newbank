90afa889ba35e40db73cb03de7e30e97586530d0
AhCabral <arthurcabral.dev@gmail.com>
2021-03-08 22:14:12 -0300

    Criando Account Section

---
+++ src/pages/Main/Components/AccountSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AccountSection() {
+    return (
+        <section className="account-section-container">
+
+        </section>
+    )
+}
+
+export default AccountSection
