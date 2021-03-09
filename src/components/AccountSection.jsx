b3e20660b37a31d30479b56d07916e9f7ab614b6
AhCabral <arthurcabral.dev@gmail.com>
2021-03-09 11:16:27 -0300

    Modificando de div para section e adicionando classe

--- src/components/AccountSection.jsx
+++ src/components/AccountSection.jsx
@@ -4,13 +4,13 @@ import '../styles/components/account-section.css'
 
 function AccountSection() {
     return (
-        <div >
+        <section className="account-section-container" >
             <div>
                 <h2>Lorem ipsum dolor sit amet</h2>
                 <h4>lorem consectueur dolor sit</h4>
                 <button>Lorem Ipsum</button>
             </div>
-        </div>
+        </section>
     )
 }
 
