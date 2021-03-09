55d906f4ed95794ec846cd46bb097513531ac440
AhCabral <arthurcabral.dev@gmail.com>
2021-03-08 23:34:12 -0300

    Removendo Classe

--- src/pages/Main/Components/AccountSection.jsx
+++ src/pages/Main/Components/AccountSection.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 
 function AccountSection() {
     return (
-        <div className="account-section-container">
-            <h2>Lorem ipsum dolor sit amet</h2>
-            <h4>lorem consectueur dolor sit</h4>
-            <button>Lorem Ipsum</button>
+        <div >
+            <div>
+                <h2>Lorem ipsum dolor sit amet</h2>
+                <h4>lorem consectueur dolor sit</h4>
+                <button>Lorem Ipsum</button>
+            </div>
         </div>
     )
 }
