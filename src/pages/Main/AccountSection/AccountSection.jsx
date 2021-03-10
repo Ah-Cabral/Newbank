bcc0cc742d693dbc37c9e0899d58dd50b051d5f5
AhCabral <arthurcabral.dev@gmail.com>
2021-03-10 20:32:03 -0300

    Organizando Pastas

---
+++ src/pages/Main/AccountSection/AccountSection.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import './account-section.css'
+
+function AccountSection() {
+    return (
+        <section className="account-section-container" >
+            <div>
+                <h2>Lorem ipsum dolor sit amet</h2>
+                <h4>lorem consectueur dolor sit</h4>
+                <button>Lorem Ipsum</button>
+            </div>
+        </section>
+    )
+}
+
+export default AccountSection
---
+++ src/pages/Main/HomeSection/HomeSection.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+import { BlackCard, PurpleCard } from '../../../components/Cards'
+
+import './home-section.css'
+
+
+function homeSection() {
+    return (
+        <section>
+            <div className="home-section-container">
+
+                <div className="gray-ball ball"></div>
+                <div className="purple-ball ball"></div>
+
+                <section className="welcome-section">
+                    <h1>Você no controle do seu dinheiro</h1>
+                    <h3>Controle total do cartão de crédito e da conta 100% digital</h3>
+                    <button>Saiba Mais</button>
+                </section>
+
+                <div className="div-card">
+                    <PurpleCard name='Arthur Cabral' />
+                </div>
+
+                {/*<BlackCard name='Bem vindo ;)' />*/}
+
+            </div>
+
+        </section>
+    )
+}
+
+export default homeSection
