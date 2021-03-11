6c61fb2cbe9abce79a33043e22c0d62954a39e24
AhCabral <arthurcabral.dev@gmail.com>
2021-03-10 23:28:15 -0300

    Account Section

--- src/pages/Main/AccountSection/AccountSection.jsx
+++ src/pages/Main/AccountSection/AccountSection.jsx
@@ -5,10 +5,15 @@ import './account-section.css'
 function AccountSection() {
     return (
         <section className="account-section-container" >
-            <div>
-                <h2>Lorem ipsum dolor sit amet</h2>
-                <h4>lorem consectueur dolor sit</h4>
-                <button>Lorem Ipsum</button>
+            <div className="content-and-image">
+                <div className="content">
+                    <h2>Lorem ipsum dolor sit amet</h2>
+                    <h4>lorem consectueur dolor sit</h4>
+                    <button>Lorem Ipsum</button>
+                </div>
+                <div className="image-woman">
+                    <img src="../../../images/woman-image.png" alt="" />
+                </div>
             </div>
         </section>
     )
