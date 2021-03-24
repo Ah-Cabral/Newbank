743c3f09a06bfd60992328b5bb810d0d2226b330
AhCabral <arthurcabral.dev@gmail.com>
2021-03-23 22:51:08 -0300

    Criando função que irá mover a tela para a seção desejada

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,17 +3,34 @@ import NubankLogo from '../images/nubank-purple-logo.svg'
 import '../styles/components/css/header.css'
 
 const Header = () => {
+
+    function dispBotao(id) {
+        if (id === null || id === undefined) {
+            return console.log('ID não especificado')
+        }
+        let moveSection = document.getElementById(id)
+        if (moveSection === null || moveSection === undefined) {
+            return console.log('Erro na MoveSection, moveSection Null ou Undefined')
+        } else {
+            scroll(moveSection.offsetTop)
+        }
+    }
+
+    function scroll(qtdPixels) {
+        window.scroll({ top: qtdPixels, behavior: 'smooth' })
+    }
+
     return (
         <header className='Main-Header'>
             <div className="Left-Side">
                 <img src={NubankLogo} alt="Logo Nubank" />
-                <button className="Pattern">Seu Nubank <i class="fas fa-chevron-right"></i></button>
-                <button className="Pattern">Conta Digital <i class="fas fa-chevron-right"></i></button>
-                <button className="Pattern">Para Você <i class="fas fa-chevron-right"></i></button>
+                <button className="Pattern" onClick={(e) => dispBotao("digital-account")}>Conta Digital <i className="fas fa-chevron-right"></i></button>
+                <button className="Pattern" onClick={(e) => dispBotao("para-voce")}>Para Você <i className="fas fa-chevron-right"></i></button>
+                <button className="Pattern" onClick={(e) => dispBotao("digital-account")}>Você no Controle <i className="fas fa-chevron-right"></i></button>
             </div>
 
             <div className="Right-Side">
-                <button className="login Pattern">Login <i class="fas fa-sign-in-alt"></i></button>
+                <button className="login Pattern">Login <i className="fas fa-sign-in-alt"></i></button>
                 <button className="Purple">Quero Ser Nubank</button>
             </div>
         </header>
