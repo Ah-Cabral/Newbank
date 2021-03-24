ae5ad24afe43b53add99cc89c79004ecd1b0562d
AhCabral <arthurcabral.dev@gmail.com>
2021-03-23 22:52:23 -0300

    Adicionando ID

--- src/pages/Main/ParaVoceSection/ParaVoceSection.jsx
+++ src/pages/Main/ParaVoceSection/ParaVoceSection.jsx
@@ -1,28 +1,47 @@
-import React, { Component } from 'react'
-import LogoNubank from '../../../images/nubank-purple-logo.svg'
-
-
-
+import React from 'react'
+import logoNubank from '../../../images/nubank-purple-logo.svg'
 import './para-voce-section.css'
 
+const ParaVoceSection = () => {
+    return (
+        <section className="para-voce-container" id="para-voce">
+            <div className="img-and-text">
+                <h2>Por quê Nubank é Perfeito Para Você?</h2>
 
-export class ParaVoceSection extends Component {
-    render() {
-        return (
-            <section className="para-voce-container">
-                <div className="img-and-text">
-                    <h2>Por quê Nubank é Perfeito Para Você?</h2>
-
-                    <div className="icons">
-                        <i class="fas fa-money-check"></i>
-                        <i class="fas fa-shield-alt"></i>
-                        <i class="far fa-clock"></i>
-                        <i class="fas fa-cog"></i>
+                <div className="icons para-voce-icons">
+                    <div>
+                        <i className="fas fa-money-check"></i>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            Amet atque soluta quaerat in corporis laboriosam consectetur.
+                        </p>
                     </div>
+                    <div>
+                        <i className="fas fa-shield-alt"></i>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            Amet atque soluta quaerat in corporis laboriosam consectetur.
+                        </p>
+                    </div>
+                    <div>
+                        <i className="far fa-clock"></i>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            Amet atque soluta quaerat in corporis laboriosam consectetur.
+                        </p>
+                    </div>
+                    <div>
+                        <i className="fas fa-cog"></i>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            Amet atque soluta quaerat in corporis laboriosam consectetur.
+                        </p>
+                    </div>
+
                 </div>
-            </section>
-        )
-    }
+            </div>
+        </section>
+    )
 }
 
 export default ParaVoceSection
