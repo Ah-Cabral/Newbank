d96f07783e9a6e64b24945ec457df9a6ca6814b1
AhCabral <arthurcabral.dev@gmail.com>
2021-04-13 11:08:46 -0300

    Criando Final Footer

---
+++ src/pages/Main/Footer/FinalFooter.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './final-footer.css'
+
+function FinalFooter() {
+    return (
+        <div className="final-footer">
+            <span>Newbank - Feito por Arthur Cabral</span>
+        </div>
+    )
+}
+
+export default FinalFooter
